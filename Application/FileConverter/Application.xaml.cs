5fb2b33d9046143e5ccf3a907a9c86c008d6c037
Jack251970 <1160210343@qq.com>
2025-08-23 14:06:23 +0800

    Bump version to 2.1.1

--- Application/FileConverter/Application.xaml.cs
+++ Application/FileConverter/Application.xaml.cs
@@ -39,7 +39,7 @@ namespace FileConverter
                                                       {
                                                           Major = 2,
                                                           Minor = 1,
-                                                          Patch = 0,
+                                                          Patch = 1,
                                                       };
 
         private bool needToRunConversionThread;
